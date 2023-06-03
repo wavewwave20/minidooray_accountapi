fa698eb86fd0014d1cab801b30048748e0deb178
pakamin77 <steelcooker17@gmail.com>
2023-06-03 17:56:57 +0900

    init

---
+++ src/main/java/com/nhnacademy/minidooray_accountapi/MinidoorayAccountapiApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.minidooray_accountapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinidoorayAccountapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinidoorayAccountapiApplication.class, args);
+    }
+
+}
